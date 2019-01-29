c4ef76b77aed6152ca64d384819d17aee0667a82
Czirjak <marton.czirjak@mmsone.com>
2019-01-29 12:52:13 +0100

    first controller

---
+++ cars/src/main/java/com/czirjak/cars/Controller.java
@@ -0,0 +1,13 @@
+package com.czirjak.cars;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping
+    public String hello() {
+        return "Hello my deployed Spring App!!";
+    }
+}
